e34ccaa324b153a12969b981e4f3587037b024a7
ubuntu18 <hoangtung796@gmail.com>
2022-07-06 06:12:28 -0700

    modifying test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,17 +3,29 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3 (ganache.provider());
+const {interface, bytecode} = require('../compile');
 
+let accounts;
+let inbox;
 
-beforeEach(() => {
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-        console.log(fetchedAccounts);       
-    });
+beforeEach(async () => {
+    //get all accounts 
+    accounts = await web3.eth.getAccounts();
     
-    
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ 
+            data: bytecode, 
+            arguments: ["hi theree"] 
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
 });
 
 describe('Inbox',() =>{ 
-    it('deploys a contract',() => {});
+    it('deploys a contract',() => {
+        console.log(inbox);
+    });
 });
