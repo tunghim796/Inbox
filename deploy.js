629d06be8f753c7e101dda4aeecca13b8791d520
tunghim <hoangtung796@gmail.com>
2022-07-07 15:34:38 +0700

    waiting for mnemnic

--- deploy.js
+++ deploy.js
@@ -1,1 +1,8 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+    
+);
