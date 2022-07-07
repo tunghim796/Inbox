5e6c90756b527c6a4ca196c5012bff39b03f814d
tunghim <hoangtung796@gmail.com>
2022-07-07 09:32:45 +0700

    nvm

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,19 +1,25 @@
-// contract test code will go here
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3 (ganache.provider());
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
+let accounts;
+let inbox;
 
-beforeEach(() => {
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-        console.log(fetchedAccounts);       
-    });
-    
-    
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe('Inbox',() =>{ 
-    it('deploys a contract',() => {});
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
 });
